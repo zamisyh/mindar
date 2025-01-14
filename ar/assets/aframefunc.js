fae1d6a515d9f61ecce6769af6c345274963f684
zamsyh.dev <zamsyh.dev@gmail.com>
2025-01-14 15:46:09 +0700

    fix bug

--- ar/assets/aframefunc.js
+++ ar/assets/aframefunc.js
@@ -1,15 +1,15 @@
-AFRAME.registerComponent("model-opacity", {
-  schema: { default: 1 },
-  init: function () {
-    this.el.addEventListener("model-loaded", () => {
-      this.el.object3D.traverse((child) => {
-        if (child.isMesh) {
-          child.material.transparent = true;
-          child.material.opacity = this.data;
-        }
-      });
-    });
-  },
-});
+// AFRAME.registerComponent("model-opacity", {
+//   schema: { default: 1 },
+//   init: function () {
+//     this.el.addEventListener("model-loaded", () => {
+//       this.el.object3D.traverse((child) => {
+//         if (child.isMesh) {
+//           child.material.transparent = true;
+//           child.material.opacity = this.data;
+//         }
+//       });
+//     });
+//   },
+// });
 
 
